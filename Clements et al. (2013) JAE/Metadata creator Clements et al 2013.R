243f7249efe7f1dcc9a615ee067d3b89125c6e32
Christopher Clements <chrisclementsresearch@gmail.com>
2019-12-13 14:10:49 +0000

    Push data from exp1 to github. no meta data as yet

---
+++ Clements et al. (2013) JAE/Metadata creator Clements et al 2013.R
@@ -0,0 +1,8 @@
+## meta data creator for Clements et al. (2013) - JAE
+rm(list=ls())
+library(devtools)
+##devtools::install_github("Exp-Micro-Ecol-Hub/dmdScheme")
+library("dmdScheme")
+
+##to start the meta data entry
+?enter_new_metadata()

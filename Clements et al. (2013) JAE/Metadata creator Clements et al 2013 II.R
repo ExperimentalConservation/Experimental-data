2b0ae2d11729ef4714693155f37f412d04a1b71f
Chris <chrisclementsresearch@gmail.com>
2019-12-17 10:07:21 +0000

    updates to meta data file
    still not working

--- Clements et al. (2013) JAE/Metadata creator Clements et al 2013 II.R
+++ Clements et al. (2013) JAE/Metadata creator Clements et al 2013 II.R
@@ -4,8 +4,8 @@ rm(list=ls())
 library(devtools)
 
 ##install various packages
-##devtools::install_github("Exp-Micro-Ecol-Hub/dmdScheme", ref = "master", build_opts = c("--no-resave-data"))
-##devtools::install_github("Exp-Micro-Ecol-Hub/emeScheme", ref = "master", build_opts = c("--no-resave-data"))
+  ##devtools::install_github("Exp-Micro-Ecol-Hub/dmdScheme", ref = "master", build_opts = c("--no-resave-data"))
+  ##devtools::install_github("Exp-Micro-Ecol-Hub/emeScheme", ref = "master", build_opts = c("--no-resave-data"))
 
 library(dmdScheme)
 library(emeScheme)
@@ -15,7 +15,11 @@ library(emeScheme)
 ##set wd
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-x<-validate("EXP 1 meta.xlsx")
+read_excel("EXP 1 meta.xlsx", validate = T)
 
-report('EXP 1 meta.xlsx', '~/Desktop/Work/Experimental-data/Clements et al. (2013) JAE', report = "html")
+scheme_make("EXP 1 meta.xlsx", overwrite=T)
+
+x<-validate("EXP 1 meta.xlsx", errorIfStructFalse=F)
+
+report('EXP 1 meta.xlsx', '~/Desktop/Work/Experimental-data/Clements et al. (2013) JAE', report = "html", errorIfStructFalse=F)
 
